39cc51ed2bf50ebd73054a711c77b009770fa8ac
Dave Meehan <dave_meehan@replicated.co.uk>
2015-04-13 21:06:51 +0100

    Update calculator.cpp

--- features/steps/calculator.cpp
+++ features/steps/calculator.cpp
@@ -3,3 +3,9 @@ IPassAStringContaining::visit(Value &value) {
     world().result = StringCalculator().add(std::to_string(value));
 
 }
+
+TheResultIs::visit(Value &value) {
+    
+    world().result.should == value;    
+    
+}
