d749a52209373736931ec92d8e233ff561c49288
Dave Meehan <dave_meehan@replicated.co.uk>
2015-04-13 09:33:17 +0100

    Update calculator.h

--- features/steps/calculator.h
+++ features/steps/calculator.h
@@ -1,1 +1,35 @@
+struct CalculatorWorld : World {
+    
+    int result;
+    
+}
 
+struct BeforeAll : Before {
+  
+    void visit() {
+        
+        setWorld(std::make_shared<CalculatorWorld>());
+        
+    }  
+    
+};
+
+struct AStringContaining : Step {
+    
+    void visit(Value &value) {
+        
+        world().result = StringCalculator().add(std::to_string(value));
+        
+    }
+  
+};
+
+struct TheResultIs : Step {
+  
+    void visit(Value &value) {
+    
+        world().result.should == value;    
+        
+    }
+  
+};
